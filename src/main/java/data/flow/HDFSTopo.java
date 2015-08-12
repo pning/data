124ecae0f01f7c8383b072e91621a158fdec3751
pning <yanoning@163.com>
2015-08-12 12:25:45 +0800

    mia data

---
+++ src/main/java/data/flow/HDFSTopo.java
@@ -0,0 +1,121 @@
+package data.flow;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.apache.storm.hdfs.bolt.HdfsBolt;
+import org.apache.storm.hdfs.bolt.format.DefaultFileNameFormat;
+import org.apache.storm.hdfs.bolt.format.DelimitedRecordFormat;
+import org.apache.storm.hdfs.bolt.format.FileNameFormat;
+import org.apache.storm.hdfs.bolt.format.RecordFormat;
+import org.apache.storm.hdfs.bolt.rotation.FileRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.TimedRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.TimedRotationPolicy.TimeUnit;
+import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
+import org.apache.storm.hdfs.bolt.sync.SyncPolicy;
+
+import data.util.RSAUtil;
+import storm.kafka.BrokerHosts;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class HDFSTopo {
+
+	public static class Decryption extends BaseRichBolt {
+
+		private static final long serialVersionUID = 1L;
+		private OutputCollector collector;
+
+		@SuppressWarnings("rawtypes")
+		public void prepare(Map stormConf, TopologyContext context,
+				OutputCollector collector) {
+			// TODO Auto-generated method stub
+			this.collector = collector;
+
+		}
+
+		public void execute(Tuple input) {
+			String line = input.getString(0);
+			try {
+				String log = RSAUtil.getLog(line);
+				collector.emit(input, new Values(log));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			collector.ack(input);
+
+		}
+
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			// TODO Auto-generated method stub
+			declarer.declare(new Fields("line"));
+
+		}
+
+	}
+
+	public static void main(String[] args) throws AlreadyAliveException,
+			InvalidTopologyException, InterruptedException {
+		// storm-kafka
+		BrokerHosts zk = new ZkHosts("master:2181,node1:2181,node2:2181");
+		SpoutConfig kafkaConf = new SpoutConfig(zk, "test", "/storm-hdfs",
+				"hdfs");
+		kafkaConf.scheme = new SchemeAsMultiScheme(new StringScheme());
+		kafkaConf.forceFromStart = false;
+		kafkaConf.zkServers = Arrays.asList(new String[] { "master", "node1",
+				"node2" });
+		kafkaConf.zkPort = 2181;
+		KafkaSpout kafkaSpout = new KafkaSpout(kafkaConf);
+
+		// storm-hdfs
+		RecordFormat format = new DelimitedRecordFormat()
+				.withFieldDelimiter("\t");
+		SyncPolicy syncPolicy = new CountSyncPolicy(1000);
+
+		FileRotationPolicy rotationPolicy = new TimedRotationPolicy(1.0f,
+				TimeUnit.HOURS);
+		// FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(10.0f,
+		// Units.MB);
+		FileNameFormat fileNameFormat = new DefaultFileNameFormat().withPath(
+				"/foo/").withExtension(".log");
+		HdfsBolt bolt = new HdfsBolt().withFsUrl("hdfs://master:9000")
+				.withFileNameFormat(fileNameFormat).withRecordFormat(format)
+				.withRotationPolicy(rotationPolicy).withSyncPolicy(syncPolicy);
+
+		// config
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("hdfs-kafka", kafkaSpout, 5);
+		builder.setBolt("hdfs-dec", new Decryption(), 3).shuffleGrouping(
+				"hdfs-kafka");
+		builder.setBolt("hdfs", bolt, 2).shuffleGrouping("hdfs-dec");
+
+		Config conf = new Config();
+		if (args != null && args.length > 0) {
+			conf.setNumWorkers(3);
+			StormSubmitter.submitTopology(args[0], conf,
+					builder.createTopology());
+		} else {
+			conf.setMaxTaskParallelism(3);
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology("hdfs", conf, builder.createTopology());
+			Thread.sleep(10000);
+			cluster.shutdown();
+		}
+	}
+}
---
+++ src/main/java/data/util/RSAUtil.java
@@ -0,0 +1,163 @@
+package data.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.crypto.Cipher;
+
+public final class RSAUtil {
+	private static String RSA = "RSA";
+	private static String PRIVATE_KEY = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKLAltvYR4r1kHgKHKPn+YhU9ESh3Skmu/cszZRW2Sh37aY9WNX1eLUTTqBRqh7Ff95t9FebV4HC7cu1huSWHkmLmgXJyhQIWHDOGabxm2N2QIiJwV/FRTgjk26cvr7fv6HLjcSAKtqjm0WAe2eR5H+83IZ9rF3F689tUS1aUnPrAgMBAAECgYBXB49kLipLp3bCif24tCJegbdBQCEIkVrwGPgxu6NSHZdqTuHDQAl1IsbnueOzor72zWtcsYfAruEEDqT8dT3qEblBSozrUyPCFN75c8m8aQbNdPzKaUbSTqH4vn3RZabtNVdPTv6/rdmi3mlHP36K+Xgwr0OvjTRibmCNVB6DSQJBAM4taxdgAPR45zZDGNAfWn4Uzd2yaZkX87aTnsskdXaqJKK1CWg5QnVboqkzeOLROXTIM19TwKJfcBvkg3zhumcCQQDKFMyidXxVWokg2SkilDDc+R/pg0Cq2WrePNlGdHhbCMWonrmo19q5Tz3Mn0AP43mmRHo3437tNwoK37md44/dAkBsbqCKy5fU+x0GgaU0F7k84+4dMW6t8FXhaKOzzLnmLbdUxwdqg+q60/WpLQPfrg+cpom2EdMW0kCgur6YWBLzAkBRysVg5Cik63YhY4QwyMZWSkdOXp+oB0QoG3zd9XpZbl2CeGwxGN0gsCAVWEe3ppywXXsSb/QJ04MvD4qzi03JAkEAyhlMXbDTCseemkZVY2ipYakj6jeuwxUa/bzi+UB+eeLTJd3rLnF5m/1LYaf/K72HVfBcSs2BP/e55KIc2Ob8jg==";
+
+	/**
+	 * 用私钥解密
+	 * 
+	 * @param encryptedData
+	 *            经过encryptedData()加密返回的byte数据
+	 * @param privateKey
+	 *            私钥
+	 * @return
+	 */
+	public static byte[] decryptData(byte[] encryptedData, PrivateKey privateKey) {
+		try {
+			Cipher cipher = Cipher.getInstance(RSA);
+			cipher.init(Cipher.DECRYPT_MODE, privateKey);
+			return cipher.doFinal(encryptedData);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * 从字符串中加载私钥<br>
+	 * 加载时使用的是PKCS8EncodedKeySpec（PKCS#8编码的Key指令）。
+	 * 
+	 * @param privateKeyStr
+	 * @return
+	 * @throws Exception
+	 */
+	public static PrivateKey loadPrivateKey(String privateKeyStr)
+			throws Exception {
+		try {
+			byte[] buffer = new sun.misc.BASE64Decoder()
+					.decodeBuffer(privateKeyStr);
+			// X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+			KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+			return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+		} catch (NoSuchAlgorithmException e) {
+			throw new Exception("无此算法");
+		} catch (InvalidKeySpecException e) {
+			System.out.println(e);
+			throw new Exception("私钥非法");
+		} catch (NullPointerException e) {
+			throw new Exception("私钥数据为空");
+		}
+	}
+
+	/**
+	 * 从文件中加载私钥
+	 * 
+	 * @param keyFileName
+	 *            私钥文件名
+	 * @return 是否成功
+	 * @throws Exception
+	 */
+	public static PrivateKey loadPrivateKey(InputStream in) throws Exception {
+		try {
+			return loadPrivateKey(readKey(in));
+		} catch (IOException e) {
+			throw new Exception("私钥数据读取错误");
+		} catch (NullPointerException e) {
+			throw new Exception("私钥输入流为空");
+		}
+	}
+
+	/**
+	 * 读取密钥信息
+	 * 
+	 * @param in
+	 * @return
+	 * @throws IOException
+	 */
+	private static String readKey(InputStream in) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String readLine = null;
+		StringBuilder sb = new StringBuilder();
+		while ((readLine = br.readLine()) != null) {
+			if (readLine.charAt(0) == '-') {
+				continue;
+			} else {
+				sb.append(readLine);
+				sb.append('\r');
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static String getValues(String key) throws Exception {
+		String k[] = key.split("=");
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < k.length; i++) {
+			PrivateKey privateKey = loadPrivateKey(PRIVATE_KEY);
+			byte[] decryptByte = decryptData(
+					new sun.misc.BASE64Decoder().decodeBuffer((k[i] + "=")
+							.replaceAll("-", "+").replaceAll("_", "/")),
+					privateKey);
+			String decryptStr = new String(decryptByte);
+			sb.append(decryptStr);
+		}
+		return sb.toString();
+	}
+
+	public static String getLog(String log) throws Exception {
+		Pattern p = Pattern
+				.compile("^\\[.*\\|\\|.*\\|\\|.*\\|\\|.*\\|\\|.*\\|\\|.*\\|.*");
+		Matcher m = p.matcher(log);
+		String shuju = null;
+		String data = null;
+		if (m.matches()) {
+			shuju = log.substring(log.indexOf("]") + 2);
+			String vr[] = shuju.split("\\|\\|")[5].split("_");
+			try {
+				if (new Integer(vr[1]) >= 3 && new Integer(vr[2]) >= 4) {
+					data = log.replace(
+							shuju.split("\\|\\|")[2],
+							getValues(shuju.split("\\|\\|")[2]).replace("\"",
+									""));
+				} else {
+
+					data = log.replace(shuju.split("\\|\\|")[2],
+							shuju.split("\\|\\|")[2].replace("\\x22", ""));
+				}
+			} catch (Exception e) {
+				data = log;
+			}
+		} else {
+			data = log;
+		}
+		return data;
+	}
+
+	// public static void main(String[] args) throws Exception {
+	// String log =
+	// "[28/Jul/2015:00:00:02 +0800] ios||/banner/listsExt/||EijKNAaZDYUKzkcFh_iRQSsiMpbpXGa-KQWQ1LSWqGzzEvhuZ-VGBO7RLvYAr-qr11O_Vutxyaw3mKbEtoV5bvFOnmzSJrrAJoBC2s7sLeOdSOTcUj91FiVgpZF2uXc3qKTciIewHJY7RUeHlW84HEq3AhTjTsW9sHoDeoPtes8=||iosOfficial||8A3E0B7F-6F37-41B4-B649-ECD361FFA321||ios_3_4_2||121.224.189.138";
+	// String log1 =
+	// "[28/Jul/2015:00:00:02 +0800] ios||/topcart/count/||||iosOfficial||0756E9CE-89B5-4999-8581-621E4D615862||ios_3_4_2||36.33.98.112";
+	// String log2 =
+	// "[28/Jul/2015:00:00:02 +0800] ios||/collect/getLists||{\\x22type\\x22:1,\\x22count\\x22:10,\\x22page\\x22:3}||iosOfficial||5FED9703-ED20-43F7-9C51-EF41161588FD||ios_3_3_3||43.224.52.0";
+	// System.out.println(getLog(log));
+	// }
+
+}
