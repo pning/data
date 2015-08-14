8f399ab76145949842faa9fb2838cdb2b1976dc1
pning <yanoning@163.com>
2015-08-14 14:26:35 +0800

    yannng

---
+++ src/main/java/data/util/Constants.java
@@ -0,0 +1,8 @@
+package data.util;
+
+public interface Constants {
+	public static final String HBASE_PROP_CONF_FILE = "parameter.properties";
+	public static final String LOG_DEC_TYPE = "RSA";
+	public static final String ZkHosts = "master:2181,node1:2181,node2:2181";
+	public static final String HDFS_PATH = "hdfs://master:9000";
+}
